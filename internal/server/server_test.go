ed66e5e9547fc6991c21dd85ad0168ee52f6c154
Kate Smith <kate.smith@digital.justice.gov.uk>
2025-10-15 17:03:33 +0100

    sfs-188 setup repo #minor
    Signed-off-by: Kate Smith <kate.smith5@justice.gov.uk>

---
+++ internal/server/server_test.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"io"
+	"net/http"
+)
+
+type mockTemplate struct {
+	executed         bool
+	executedTemplate bool
+	lastVars         interface{}
+	lastW            io.Writer
+	error            error
+}
+
+func (m *mockTemplate) Execute(w io.Writer, vars any) error {
+	m.executed = true
+	m.lastVars = vars
+	m.lastW = w
+	return m.error
+}
+
+func (m *mockTemplate) ExecuteTemplate(w io.Writer, name string, vars any) error {
+	m.executedTemplate = true
+	m.lastVars = vars
+	m.lastW = w
+	return m.error
+}
+
+type mockRoute struct { //nolint:golint,unused
+	client   ApiClient
+	data     any
+	executed bool
+	lastW    io.Writer
+	error
+}
+
+func (r *mockRoute) Client() ApiClient { //nolint:golint,unused
+	return r.client
+}
+
+func (r *mockRoute) execute(w http.ResponseWriter, req *http.Request, data any) error { //nolint:golint,unused
+	r.executed = true
+	r.lastW = w
+	r.data = data
+	return r.error
+}
