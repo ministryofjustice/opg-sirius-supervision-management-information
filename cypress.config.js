a84df4d66cc22ea3bcbde07af66ec37b3900f495
Kate Smith <kate.smith5@justice.gov.uk>
2025-10-28 11:51:56 +0000

    sfs-188 configure e2e accessibility test

--- cypress.config.js
+++ cypress.config.js
@@ -8,5 +8,11 @@ module.exports = defineConfig({
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'http://localhost:7777/management-information',
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
+    screenshotsFolder: "cypress/screenshots",
+    supportFile: "cypress/support/e2e.ts",
+    modifyObstructiveCode: false,
   },
+  viewportWidth: 1000,
+  viewportHeight: 1000,
 })
