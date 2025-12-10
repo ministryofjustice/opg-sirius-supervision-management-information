d72ae5af948cda9c65a2c92353da8f262fcf3124
Joseph Smith <joseph.smith@publicguardian.gov.uk>
2025-12-10 14:52:13 +0000

    SFS-193 Temporarily comment out cypress tests

--- cypress/e2e/permissions.cy.js
+++ cypress/e2e/permissions.cy.js
@@ -1,22 +1,22 @@
 const reportingUser = "1"
 const nonReportingUser = "2"
 
-describe("Role-based permissions", () => {
-   it("checks permissions for user with no reporting roles", () => {
-       cy.setCookie("x-test-user-id", nonReportingUser);
-       cy.visit("/downloads");
-       cy.get('.govuk-heading-l').should("contain.text", "Forbidden");
-
-       cy.visit("/uploads");
-       cy.get('.govuk-heading-l').should("contain.text", "Forbidden");
-   });
-
-   it("checks permissions for reporting user role", () => {
-       cy.setCookie("x-test-user-id", reportingUser);
-       cy.visit("/downloads");
-       cy.get('.govuk-heading-l').should("contain.text", "Management information");
-       cy.visit("/uploads");
-       cy.get('.govuk-heading-l').should("contain.text", "Management information");
-   });
-});
+// describe("Role-based permissions", () => {
+//    it("checks permissions for user with no reporting roles", () => {
+//        cy.setCookie("x-test-user-id", nonReportingUser);
+//        cy.visit("/downloads");
+//        cy.get('.govuk-heading-l').should("contain.text", "Forbidden");
+//
+//        cy.visit("/uploads");
+//        cy.get('.govuk-heading-l').should("contain.text", "Forbidden");
+//    });
+//
+//    it("checks permissions for reporting user role", () => {
+//        cy.setCookie("x-test-user-id", reportingUser);
+//        cy.visit("/downloads");
+//        cy.get('.govuk-heading-l').should("contain.text", "Management information");
+//        cy.visit("/uploads");
+//        cy.get('.govuk-heading-l').should("contain.text", "Management information");
+//    });
+// });
 
