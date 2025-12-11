426c20e79f0ca25105df7db04b538b53fad61400
Joseph Smith <joseph.smith@publicguardian.gov.uk>
2025-12-11 12:06:25 +0000

    SFS-193 Fix failing build

--- management-information/internal/server/get_uploads_test.go
+++ management-information/internal/server/get_uploads_test.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"github.com/opg-sirius-supervision-management-information/management-information/internal/model"
 	"github.com/opg-sirius-supervision-management-information/shared"
 	"github.com/stretchr/testify/assert"
 	"net/http"
